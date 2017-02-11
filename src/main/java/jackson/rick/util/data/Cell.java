b3d89f093db27bedf22ed4503684c6bc020e92cb
rjackson88 <richardwjacksonjr@yahoo.com>
2017-02-10 20:26:50 -0500

    0.0.2

--- src/main/java/jackson/rick/util/data/Cell.java
+++ src/main/java/jackson/rick/util/data/Cell.java
@@ -3,5 +3,9 @@ package jackson.rick.util.data;
 /**
  * Created by rickjackson on 2/10/17.
  */
-public class Cell {
+class Cell {
+    
+    Cell() {
+        
+    }
 }
--- src/main/java/jackson/rick/util/data/Record.java
+++ src/main/java/jackson/rick/util/data/Record.java
@@ -1,7 +1,27 @@
 package jackson.rick.util.data;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by rickjackson on 2/10/17.
  */
-public class Record {
+class Record<K, V> {
+    private HashMap<Integer, Cell> cells;
+    
+    public Record() {
+        this.cells = new HashMap<>();
+    }
+    
+    public Record(int initialCapacity, float loadFactor) {
+        this.cells = new HashMap<>(initialCapacity, loadFactor);
+    }
+    
+    public Record(int initialCapacity) {
+        
+    }
+    
+    public Record(Map<? extends K, ? extends V> m) {
+        
+    }
 }
